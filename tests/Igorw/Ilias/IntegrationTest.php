12c808733ccaa8ad370086f502fbd18ff812c4de
Igor Wiedler <igor@wiedler.ch>
2012-11-30 03:04:57 +0100

    DRY IntegrationTest

--- tests/Igorw/Ilias/IntegrationTest.php
+++ tests/Igorw/Ilias/IntegrationTest.php
@@ -16,10 +16,43 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
         $this->env = Environment::standard();
     }
 
-    /** @test */
-    public function evaluateValue()
+    /**
+     * @test
+     * @dataProvider provideEvaluate
+     */
+    public function evaluate($expected, $code, $vars = array())
     {
-        $this->assertSame(3, $this->program->evaluate('(+ 1 2)', $this->env));
+        foreach ($vars as $name => $value) {
+            $this->env[$name] = $value;
+        }
+
+        $this->assertSame($expected, $this->program->evaluate($code, $this->env));
+    }
+
+    public function provideEvaluate()
+    {
+        return [
+            'value'                 => [3, '(+ 1 2)'],
+            'variable'              => [42, 'foo', ['foo' => 42]],
+            'quoted string'         => ['foo', "'foo"],
+            'quoted list'           => [['foo'], "'(foo)"],
+            'nested quoted list'    => [[['foo']], "'((foo))"],
+            'lambda'                => [
+                42,
+                '(define identity (lambda (x) x)) (identity 42)',
+            ],
+            'lambda with real body' => [
+                51,
+                '(define add-one (lambda (x) (+ x 1))) (add-one 50)',
+            ],
+            'addition'              => [100, '(+ 1 2 3 94)'],
+            'subtraction'           => [194, '(- 200 1 2 3)'],
+            'if'                    => [2, '(if 1 2 3)'],
+            'if with real body'     => [2, '(if 1 (+ 1 1) (+ 1 1 1))'],
+            'if with false cond'    => [3, '(if 0 (+ 1 1) (+ 1 1 1))'],
+            'if without else'       => [null, '(if 0 2)'],
+            'if with cond body'     => [3, '(if (- 1 1) 2 3)'],
+        ];
     }
 
     /** @test */
@@ -27,87 +60,6 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
     {
         $this->program->evaluate('(define foo 42)', $this->env);
         $this->assertSame(42, $this->env['foo']);
-    }
-
-    /** @test */
-    public function evaluateVariable()
-    {
-        $this->env['foo'] = 42;
-        $this->assertSame(42, $this->program->evaluate('foo', $this->env));
-    }
-
-    /** @test */
-    public function evaluateQuotedString()
-    {
-        $this->assertSame('foo', $this->program->evaluate("'foo", $this->env));
-    }
-
-    /** @test */
-    public function evaluateQuotedList()
-    {
-        $this->assertSame(['foo'], $this->program->evaluate("'(foo)", $this->env));
-    }
-
-    /** @test */
-    public function evaluateNestedQuotedList()
-    {
-        $this->assertSame([['foo']], $this->program->evaluate("'((foo))", $this->env));
-    }
-
-    /** @test */
-    public function evaluateLambda()
-    {
-        $this->program->evaluate('(define identity (lambda (x) x))', $this->env);
-        $this->assertSame(42, $this->program->evaluate('(identity 42)', $this->env));
-    }
-
-    /** @test */
-    public function evaluateLambdaWithRealBody()
-    {
-        $this->program->evaluate('(define add-one (lambda (x) (+ x 1)))', $this->env);
-        $this->assertSame(51, $this->program->evaluate('(add-one 50)', $this->env));
-    }
-
-    /** @test */
-    public function evaluateAdditionArithmetic()
-    {
-        $this->assertSame(100, $this->program->evaluate('(+ 1 2 3 94)', $this->env));
-    }
-
-    /** @test */
-    public function evaluateSubtractionArithmetic()
-    {
-        $this->assertSame(194, $this->program->evaluate('(- 200 1 2 3)', $this->env));
-    }
-
-    /** @test */
-    public function evaluateIf()
-    {
-        $this->assertSame(2, $this->program->evaluate('(if 1 2 3)', $this->env));
-    }
-
-    /** @test */
-    public function evaluateIfWithRealBody()
-    {
-        $this->assertSame(2, $this->program->evaluate('(if 1 (+ 1 1) (+ 1 1 1))', $this->env));
-    }
-
-    /** @test */
-    public function evaluateIfWithFalseCondition()
-    {
-        $this->assertSame(3, $this->program->evaluate('(if 0 (+ 1 1) (+ 1 1 1))', $this->env));
-    }
-
-    /** @test */
-    public function evaluateIfWithoutElse()
-    {
-        $this->assertSame(null, $this->program->evaluate('(if 0 2)', $this->env));
-    }
-
-    /** @test */
-    public function evaluateIfWithConditionBody()
-    {
-        $this->assertSame(3, $this->program->evaluate('(if (- 1 1) 2 3)', $this->env));
     }
 
     /** @test */
