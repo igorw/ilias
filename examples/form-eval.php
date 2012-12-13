bed158ff3435b7bde8bb200a0dd6c27f1369f000
Igor Wiedler <igor@wiedler.ch>
2012-12-13 02:32:01 +0100

    Add mandatory get-random-number

--- examples/form-eval.php
+++ examples/form-eval.php
@@ -8,11 +8,15 @@ $ast = [
     ['+', 1, 2],
     ['+', 1, ['+', 1, 1]],
     [['get-plus-func'], 1, 2],
+    ['get-random-number'],
 ];
 
 $env = Environment::standard();
 $env['get-plus-func'] = function () use ($env) {
     return $env['+'];
+};
+$env['get-random-number'] = function () {
+    return 4;
 };
 
 $builder = new FormTreeBuilder();
