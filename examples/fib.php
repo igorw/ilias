2677c6174e3be0e8f47cf5bc90fec6252660e508
Igor Wiedler <igor@wiedler.ch>
2012-12-14 06:29:58 +0100

    Add fib example script

---
+++ examples/fib.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Igorw\Ilias;
+
+require __DIR__.'/../vendor/autoload.php';
+
+if (1 === $argc) {
+    echo "Usage: php examples/fib.php NUMBER\n";
+    exit(1);
+}
+
+$program = new Program(
+    new Lexer(),
+    new Reader(),
+    new FormTreeBuilder()
+);
+
+$env = Environment::standard();
+
+$code = '(define fib (lambda (n)
+    (if (< n 2)
+        n
+        (+ (fib (- n 1)) (fib (- n 2))))))';
+$program->evaluate($env, $code);
+
+$code = sprintf('(fib %s)', (int) $argv[1]);
+$value = $program->evaluate($env, $code);
+var_dump($value);
