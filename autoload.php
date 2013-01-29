c3219381ce9ff08be38b79c88ae78462bf3fba25
Igor Wiedler <igor@wiedler.ch>
2013-01-29 22:16:20 +0100

    Extract base path to variable

--- autoload.php
+++ autoload.php
@@ -2,7 +2,8 @@
 
 spl_autoload_register(function($className) {
     $prefix = 'Igorw\\Ilias\\';
+    $basePath = __DIR__.'/src/';
     if (strncmp($prefix, $className, strlen($prefix)) === 0) {
-        require __DIR__.'/src/'.str_replace('\\', '/', substr($className, strlen($prefix))).'.php';
+        require $basePath.str_replace('\\', '/', substr($className, strlen($prefix))).'.php';
     }
 });
