eb8ed128b3b456f095515c9ee6f179733e8a9495
Igor Wiedler <igor@wiedler.ch>
2013-01-29 22:10:43 +0100

    Simplified autoloader by @edorian

--- autoload.php
+++ autoload.php
@@ -1,29 +1,10 @@
 <?php
 
-spl_autoload_register(function ($className) {
-    $basePath = __DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR;
-    $classPrefix = 'Igorw\\Ilias';
-    if (null === $classPrefix || 0 !== strpos($className, $classPrefix)) {
+spl_autoload_register(function($className) {
+    $prefix = 'Igorw\\Ilias\\';
+    if (strncmp($prefix, $className, strlen($prefix)) !== 0) {
         return;
     }
-
-    $className = ltrim($className, '\\');
-    $fileName  = '';
-    $namespace = '';
-    if ($lastNsPos = strrpos($className, '\\')) {
-        $namespace = substr($className, 0, $lastNsPos);
-        $className = substr($className, $lastNsPos + 1);
-        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-    }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-
-    if (null !== $classPrefix) {
-        $transformedClassPrefix = ('\\' === substr($classPrefix, -1)) ? $classPrefix : $classPrefix.'\\';
-        $transformedClassPrefix = str_replace('\\', DIRECTORY_SEPARATOR, $transformedClassPrefix);
-        if (0 === strpos($fileName, $transformedClassPrefix)) {
-            $fileName = substr($fileName, strlen($transformedClassPrefix));
-        }
-    }
-
-    require $basePath.$fileName;
+    $file = __DIR__.'/src/'.str_replace('\\', '/', substr($className, strlen($prefix))).'.php';
+    require $file;
 });
