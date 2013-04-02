3646fea9799c6bee8808a052dee2190744aca4a8
Igor Wiedler <igor@wiedler.ch>
2013-04-02 21:41:21 +0200

    Add y-combinator example, ported from lisphp

---
+++ examples/y-combinator.php
@@ -0,0 +1,24 @@
+<?php
+
+// Y-combinator
+// thanks to the little schemer
+
+$code = <<<EOF
+(define Y
+    (lambda (le)
+        ((lambda (f) (f f))
+            (lambda (f)
+                (le (lambda (x) ((f f) x)))))))
+
+(define length
+    (Y (lambda (length)
+        (lambda (l)
+            (cond
+                ((eq? l '()) 0)
+                (#else (+ 1 (length (cdr l)))))))))
+
+(length '(a b c))
+EOF;
+
+require __DIR__.'/_run.php';
+run($code);
